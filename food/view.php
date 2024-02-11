78bf45f6838f73affd8279f5fee5e453f207357c
taher taherabbas <taherabbasaass@gmail.com>
2024-02-11 09:21:24 +0300

    first commit

--- food/view.php
+++ food/view.php
@@ -1,9 +1,12 @@
 <?php
 include "../connect.php";
 
-
-$stmt = $con->prepare("SELECT * FROM `category`");
-
+$stmt = $con->prepare("
+    SELECT m.MenuItemID, m.Name AS MenuItemName, GROUP_CONCAT(i.url_image) AS Images
+    FROM menuitems m
+    LEFT JOIN images i ON m.MenuItemID = i.id_items
+    GROUP BY m.MenuItemID, m.Name
+");
 
 $stmt->execute();
 
@@ -12,8 +15,12 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $count = $stmt->rowCount();
 
 if ($count > 0) {
+    foreach ($data as &$row) {
+        // تقسيم النص في الحقل "Images" إلى عناوين فردية باستخدام الفاصل ","
+        $row["Images"] = explode(",", $row["Images"]);
+    }
     echo json_encode(array("status" => "success", "data" => $data));
 } else {
-
     echo json_encode(array("status" => "fail"));
 }
+?>

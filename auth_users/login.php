6490013dbcca584656b079d773309254032b2d1e
taher taherabbas <taherabbasaass@gmail.com>
2024-03-03 12:02:44 +0300

    first commit

--- auth_users/login.php
+++ auth_users/login.php
@@ -1,19 +1,16 @@
 <?php
 include "../connect.php";
-$username = filterRequest("user_name");
-$password = filterRequest("password");
+$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-$stmt = $con->prepare("SELECT * FROM admin WHERE password= ?  AND user_name= ?");
-
-
-$stmt->execute(array($password, $username));
-
+$stmt = $con->prepare("SELECT id,name,email,phone FROM users WHERE password = ? AND email = ?");
+$stmt->execute(array($password, $email));
 $count = $stmt->rowCount();
 
-
 if ($count > 0) {
-    echo json_encode(array("status" => "success", array($stmt["id"])));
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(array("status" => "success", "data" => $data));
 } else {
-
     echo json_encode(array("status" => "fail"));
 }
+?>
--- auth_users/signup.php
+++ auth_users/signup.php
@@ -12,8 +12,12 @@ $stmt->execute();
 $count = $stmt->rowCount();
 
 
+$stmt = $con->prepare("SELECT id,name,email,phone FROM users WHERE password = ? AND email = ?");
+$stmt->execute(array($password, $email));
+$count = $stmt->rowCount();
 if ($count > 0) {
-    echo json_encode(array("status" => "success"));
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(array("status" => "success", "data" => $data));
 } else {
 
     echo json_encode(array("status" => "fail"));
